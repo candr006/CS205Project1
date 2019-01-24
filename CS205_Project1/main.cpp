e2beebd2a57c4db1ee2643b307d389356b62bc4c
candr006 <candr006@ucr.edu>
2019-01-23 20:38:07 -0800

    start work on generating neighbors

--- CS205_Project1/main.cpp
+++ CS205_Project1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <set>
 using namespace std;
 //change this value to make this work for a puzzle of different size
 const int puzzle_size=8;
@@ -10,22 +11,42 @@ const int dim=((puzzle_size)/2)-1;
 struct Node{
     int state[dim][dim];
     int path_cost=0;
+    //keep track of where the blank is
+    pair<int,int> pos_blank;
     //Add comparator to enable priority queue sorting by path_cost
-    bool operator()(const Node& a, const Node& b)const {
-        return a.path_cost<b.path_cost;
+    bool operator<(const Node& b)const {
+        return this->path_cost<b.path_cost;
     }
 
 
 };
 
-void setStatesEqual(int (&a)[dim][dim], int (b)[dim][dim]){
+pair<int,int> setStatesEqual(int (&a)[dim][dim], int (b)[dim][dim]){
+    pair<int,int> pos_blank;
     for( int i=0; i<dim; i++){
         for(int j=0; j<dim; j++){
             a[i][j]=b[i][j];
+
+            if(a[i][j]==0){
+                pos_blank.first=i;
+                pos_blank.second=j;
+            }
         }
     }
 
-    return;
+    return pos_blank;
+}
+
+bool checkStatesEqual(int (&a)[dim][dim], int (b)[dim][dim]){
+    for( int i=0; i<dim; i++){
+        for(int j=0; j<dim; j++){
+            if(a[i][j]!=b[i][j]){
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 void setNodesEqual(Node &a, Node b){
@@ -35,22 +56,102 @@ void setNodesEqual(Node &a, Node b){
     return;
 }
 
+//this function returns the neighbor of the state passed in with the blank moved
+Node gen_neighbor(string movement, int (a)[dim][dim], pair<int, int> b_pos){
+    int temp[dim][dim];
+    Node n;
+   /* if(movement=="up"){
+        temp=a[(b_pos.first)-1][b_pos.second];
+       Node n;
+       pair<int,int> bpos= setStatesEqual(n.state,temp);
+    }
+    else if(movement=="down"){
+        temp=a[(b_pos.first)+1][b_pos.second];
+        a[b_pos.first][b_pos.second]=temp;
+        a[(b_pos.first)+1][b_pos.second]=0;
+    }
+    else if(movement=="right"){
+        temp=a[(b_pos.first)][b_pos.second+1];
+        a[b_pos.first][b_pos.second]=temp;
+        a[(b_pos.first)][b_pos.second+1]=0;
+
+    }
+    else if(movement=="left"){
+        temp=a[(b_pos.first)][b_pos.second-1];
+        a[b_pos.first][b_pos.second]=temp;
+        a[(b_pos.first)][b_pos.second-1]=0;
+    }*/
 
 
-void UniformCostSearch(int (puzzle)[dim][dim]){
+    return n;
+}
+
+
+//this function returns all possible neighbors of the node passed in
+//only legal moves are considered
+vector<Node> getNeighbors(Node n){
+    vector<Node> neighbors;
+
+    if(n.pos_blank.first>0){
+        //move blank up possible
+        neighbors.push_back(gen_neighbor("up", n.state, n.pos_blank));
+
+
+
+/*
+        neighbor_temp.parent=&current;
+
+
+        //if the node is not already in the open set, push it onto it
+        found_iter=found(open, neighbor_temp);
+        if(!found_iter.first){
+            //Assign f_val
+            if(choice==1)
+                neighbor_temp.f_val=neighbor_temp.g_val;
+            else if(choice==2){
+                neighbor_temp.f_val=neighbor_temp.g_val+misplaced_tiles(neighbor_temp.state);
+            }
+            else if(choice==3){
+                neighbor_temp.f_val=neighbor_temp.g_val+manhattan_heuristic(neighbor_temp.state);
+            }
+            open.push_back(neighbor_temp);
+*/
+        }
+
+
+
+        return neighbors;
+}
+
+void UniformCostSearch(int (puzzle)[dim][dim], int (goal)[dim][dim]){
     Node initial_state;
     priority_queue<Node> frontier;
-    //Add initial_state to frontier priority queue
-    frontier.push(initial_state);
+    //set<int[dim][dim]> explored;
+    set<Node> explored;
 
     //set initial Node state to the puzzle
-    setStatesEqual(initial_state.state,puzzle);
+    pair<int,int> pblank = setStatesEqual(initial_state.state,puzzle);
+    initial_state.pos_blank=pblank;
+
+    //Add initial_state to frontier priority queue
+    frontier.push(initial_state);
 
 
     do {
         //Lowest cost Node
         Node n=frontier.top();
         frontier.pop();
+
+        //if the state that was popped is equal to the goal state, then we are done
+        if(checkStatesEqual(n.state,goal)){
+            cout<< "Solution Found" << endl;
+            return;
+        }
+        //otherwise, add the state to those that have been explored
+        explored.insert(n);
+
+        //find all neighbors of current state, n (max 4)
+        //possible actions are move up, down, left, right
     }
     while(!frontier.empty());
     if(frontier.empty()){
@@ -64,9 +165,26 @@ void UniformCostSearch(int (puzzle)[dim][dim]){
 }
 
 void general_search(int puzzle[dim][dim], int alg_choice){
+    //goal state
+    int count=1;
+    int goal[dim][dim];
+    for(int i=0; i<dim; i++){
+        for(int j=0; j<dim; j++){
+            if (count<(puzzle_size+1)){
+                goal[i][j]= count;
+                count++;
+            }
+            else
+                goal[i][j]= 0;
+
+        }
+    }
+
+
+
     switch(alg_choice){
         case 1:
-            UniformCostSearch(puzzle);
+            UniformCostSearch(puzzle,goal);
             break;
     };
 
@@ -79,7 +197,7 @@ int main(){
     int puzzleinput[dim][dim];
 
     cout<<"Welcome to Bertie Woosters 8-puzzle solver.\n"
-          "Type “1” to use a default puzzle, or “2” to enter your own puzzle.";
+          "Type “1” to use a default puzzle, or “2” to enter your own puzzle.\n";
     cin>>userchoice;
 
     if(userchoice==1){
@@ -112,7 +230,7 @@ int main(){
         cout <<"Enter your choice of algorithm\n"
                "1.\tUniform Cost Search\n"
                "2.\tA* with the Misplaced Tile heuristic.\n"
-               "3.\tA* with the Manhattan distance heuristic.";
+               "3.\tA* with the Manhattan distance heuristic.\n";
         cin>>alg_choice;
 
 
